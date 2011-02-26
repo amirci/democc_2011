a8db319f59ae2af13fe9c451ba94e7e46d161a2d
Amir Barylko <amir@barylko.com>
2011-02-26 03:30:13 -0600

    Added setup as dependency, fixed issue with include REXML

--- Rakefile.rb
+++ Rakefile.rb
@@ -15,21 +15,18 @@ CLEAN.include("main/**/bin", "main/**/obj", "test/**/obj", "test/**/bin")
 CLOBBER.include("**/_*", "lib/*", "**/*.user", "**/*.cache", "**/*.suo")
 
 desc 'Default build'
-task :default => ["build:all"]
+task :default => ["setup", "build:all"]
 
 desc 'Setup requirements to build and deploy'
-task :setup => ["setup:dep:local"]
+task :setup => ["setup:local"]
 
 namespace :setup do
-	namespace :dep do
-		Noodle::Rake::NoodleTask.new :local do |n|
-			n.groups << :runtime
-			n.groups << :dev
-		end
+	Noodle::Rake::NoodleTask.new :local do |n|
+		n.groups << :dev
 	end
 end
 
-namespace :build do
+namespace :build  do
 
 	desc "Build the project"
 	msbuild :all, :config do |msb, args|
@@ -58,10 +55,9 @@ task :commit => ["tools:stylecop", :test] do
 	sh '"C:\Program Files\TortoiseSVN\bin\TortoiseProc.exe" /command:commit /path:"." /notempfile'
 end
 
-require 'rexml/document'
-include REXML
-
 namespace :tools do
+	require 'rexml/document'
+
 	desc "Runs stylecop and generates a report on the Output folder"
 	task :stylecop do
 		mkdir "Output" unless File.directory? "Output"
@@ -70,8 +66,8 @@ namespace :tools do
 		sh "#{stylecop} -sf #{solution_file} -ifp AssemblyInfo.cs -of output/stylecop.xml -sc Settings.StyleCop -tf tools/StyleCopCmd/StyleCopReport.xsl"
 		sh "tools/Xslt/msxsl.exe Output/stylecop.violations.xml tools/stylecopcmd/ViolationsReport.xsl -o Output/StyleCop.Violations.html"
 
-		xmldoc = Document.new(File.new("Output/stylecop.violations.xml"))
-		violations = XPath.first(xmldoc, "/StyleCopViolations/Violation")
+		xmldoc = REXML::Document.new(File.new("Output/stylecop.violations.xml"))
+		violations = REXML::XPath.first(xmldoc, "/StyleCopViolations/Violation")
 		abort "Stylecop Failed! Please check output\stylecop.violations.html!" unless violations.nil?
 	end
 end
