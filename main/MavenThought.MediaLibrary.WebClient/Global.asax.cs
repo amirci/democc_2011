f4c17610030780f6e43829b790bbb909f29095ed
Amir Barylko <amir@barylko.com>
2011-05-24 14:07:39 -0500

    Forgot adding files

--- main/MavenThought.MediaLibrary.WebClient/Global.asax.cs
+++ main/MavenThought.MediaLibrary.WebClient/Global.asax.cs
@@ -56,7 +56,6 @@ namespace MavenThought.MediaLibrary.WebClient
             this.Container.Register(
                 Component.For<IControllerFactory>().ImplementedBy<WindsorControllerFactory>(),
                 Component.For<IWindsorContainer>().Instance(this.Container));
-
         }
     }
 }
